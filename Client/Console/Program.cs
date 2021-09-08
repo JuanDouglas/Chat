fa091189fe86d983a24df59961ab2141cc2b3655
Juan Douglas <juandouglas2004@gmail.com>
2021-09-08 14:28:10 -0300

    Client Console

---
+++ Client/Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chat.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
